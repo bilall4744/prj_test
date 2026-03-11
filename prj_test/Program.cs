70414b97f5f5f7a7e3ff2dbefa0888444eea27ca
klabukov496 <klabukov496@gmail.com>
2026-03-11 14:43:39 +0300

    second commit

--- prj_test/Program.cs
+++ prj_test/Program.cs
@@ -11,9 +11,9 @@ namespace prj_test
         static void Main(string[] args)
         {
             Console.WriteLine("Hello prj_test_1 ...");
-            Console.WriteLine("Hello 10 ...");
-            Console.WriteLine("Hello 20 ...");
-            Console.WriteLine("Hello 30 ...");
+            Console.WriteLine("Hello 11 ...");
+            Console.WriteLine("Hello 21 ...");
+            Console.WriteLine("Hello 31 ...");
         }
     }
 }
