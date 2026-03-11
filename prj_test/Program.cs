dc041f1f89e531a8dfbd1a3e15723a0ab7b334e5
klabukov496 <klabukov496@gmail.com>
2026-03-11 14:26:37 +0300

    Добавьте файлы проекта.

---
+++ prj_test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prj_test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello prj_test_1 ...");
+            Console.WriteLine("Hello 1 ...");
+            Console.WriteLine("Hello 2 ...");
+            Console.WriteLine("Hello 3 ...");
+        }
+    }
+}
